4c31528b96513d7bef081e337c90684c45f580a5
Arthur-GitEst <arthuresterqui@outlook.com>
2024-05-06 09:59:59 -0300

    Aula 06/05

--- FPR/lista-8/questao-6/questao-6.c
+++ FPR/lista-8/questao-6/questao-6.c
@@ -2,3 +2,31 @@
 // recursiva que determine se s é ou não um
 // palíndromo.
 
+#include <stdio.h>
+#include <string.h>
+
+int determinarPalindromo (char s[], int i, int j);
+
+int main () {
+    char s[] = "arara";
+    int valorInicial = 0, valorFinal = strlen(s) - 1;
+
+    if (determinarPalindromo(s, valorInicial, valorFinal)) {
+        printf("E palindromo");
+    } else {
+        printf("Nao e palindromo");
+    }
+    
+    return 0;
+}
+
+int determinarPalindromo (char s[], int i, int j) {
+    if (i < j) {
+        if (s[i] != s[j]) {
+            return 0;
+        } else {
+            determinarPalindromo(s, i+1, j-1);
+        }
+    }
+    return 1;    
+}
