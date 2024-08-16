4cb9f48f92950cecf8eef38ea86f777bd01dc9ae
Arthur-GitEst <arthuresterqui@outlook.com>
2024-08-16 11:29:00 -0300

    Aula 16/08/24

---
+++ ESD/aula-1/aula-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "minhasfuncoes.h"
+
+//criar funcao que preencha os nomes no vetor, uma funcao que exibee uma funcao que ordena
+//utilizar bubble sort
+
+int main () {
+    int **Vnomes;
+    
+    return 0;
+}
---
+++ ESD/aula-1/minhasfuncoes.h
@@ -0,0 +1,5 @@
+void detAntSuc (int x, int *ant, int *suc) {
+    *ant = x - 1;
+    *suc = x + 1;
+    return;
+}
