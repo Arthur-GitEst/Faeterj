315ec2ba31a26776277aa9dbea973040753cd770
Arthur-GitEst <arthuresterqui@outlook.com>
2024-08-22 19:15:32 -0300

    Casa 22/08/24

--- ESD/exercicio-1/exercicio-1.c
+++ ESD/exercicio-1/exercicio-1.c
@@ -6,6 +6,14 @@ int main () {
     char *vNomesE[quantidade];
     char **vNomesD = (char**)malloc(quantidade * sizeof(char*));
 
+    preencherDinamico(vNomesD, quantidade);
+
+    exibir(vNomesD, quantidade);
+
+    ordenar(vNomesD, quantidade);
+
+    exibir(vNomesD, quantidade);
+
     return 0;
 }
 
--- ESD/exercicio-1/functions.h
+++ ESD/exercicio-1/functions.h
@@ -27,9 +27,33 @@ void preencherDinamico (char **vNomes, int quant) {
   }
 }
 
+void trocar (char **n1, char **n2) {
+  char *temp = *n1;
+  *n1 = *n2;
+  *n2 = temp;
+}
+
+void ordenar (char **Vnomes, int quant) {
+  int i, j, aux;
+
+  for (i = 0; i < quant - 1; i++) {
+    aux = 0;
+    for (j = 0; j < quant - i - 1; j++) {
+      if (strcmp(Vnomes[j], Vnomes[j+1]) > 0) {
+        trocar(&Vnomes[j], &Vnomes[j+1]);
+        aux = 1;
+      }
+    }
+    if (aux == 0) {
+      break;
+    }
+  }
+}
+
 void exibir (char **vNomes, int quant) {
     int i;
 
+    printf("\n");
     for (i = 0; i < quant; i++) {
         printf("%s\n", vNomes[i]);
     }
